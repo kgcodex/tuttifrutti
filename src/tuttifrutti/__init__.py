7560f0402a4ed703c910cd4916befd73dcab620b
kgcodex <hexcodex101@gmail.com>
2026-06-29 02:00:13 +0530

    fix: import syntax

--- src/tuttifrutti/__init__.py
+++ src/tuttifrutti/__init__.py
@@ -5,13 +5,13 @@ from typing import TYPE_CHECKING, Any
 __version__ = version("tuttifrutti")
 
 if TYPE_CHECKING:
-    from ._optional_chain import OptionalChain
+    from .optional_chain import OptionalChain
 
 __all__ = ["OptionalChain"]
 
 
 def __getattr__(name: str) -> Any:
     if name == "OptionalChain":
-        return import_module("._optional_chain", __name__).OptionalChain
+        return import_module(".optional_chain", __name__).OptionalChain
 
     raise AttributeError(f"module '{__name__}' has not attribute {name}")
